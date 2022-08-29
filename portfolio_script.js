9b38e4414a688d4c04d9e752f5237f876cc3adef
musfira <musfirabadriya@gmail.com>
2022-08-30 00:06:25 +0400

    First Page Modified

--- portfolio_script.js
+++ portfolio_script.js
@@ -1,4 +1,36 @@
 $(document).ready(function()
 {
-    $("main").addClass("animated bounce");
+    /*$("main").addClass("animated bounce");*/
+    $("#resume-ball").hover(
+        function()
+        {
+            $("#resume-ball").css("background-color","white").css("color","black");   
+        },
+        function()
+        {
+            $("#resume-ball").css("background-color","rgb(255, 165, 0)");
+        }
+    );
+    $("#projects-ball").hover(
+        function()
+        {
+            $("#projects-ball").css("background-color","white").css("color","black");   
+        },
+        function()
+        {
+            $("#projects-ball").css("background-color","rgb(255, 55, 29)");
+        }
+    );
+    $("#contact-ball").hover(
+        function()
+        {
+            $("#contact-ball").css("background-color","white").css("color","black");  
+             
+        },
+        function()
+        {
+            $("#contact-ball").css("background-color","cyan");
+        }
+    );
+    $('main').scroll();
 });
